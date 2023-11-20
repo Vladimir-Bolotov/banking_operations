7e3770aaaa0eee5aa197c4a4c8be6e76e2136a32
Vladimir-Bolotov <moru4ok.89@yandex.ru>
2023-11-20 17:59:03 +0300

    add def reading_operations

--- functions.py
+++ functions.py
@@ -1,3 +1,13 @@
+import json
 
+
+def reading_operations():
+    """
+    Читает файл operations.json
+    :return: список операций
+    """
+    with open('operations.json', 'r', encoding='utf-8') as file:
+        operations = json.load(file)
+        return operations
 
 
